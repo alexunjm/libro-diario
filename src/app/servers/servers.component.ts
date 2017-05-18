2e17d1a080f6f52e6a0e34637108336c5cf54feb
alexun <alexunjm@gmail.com>
2017-05-18 13:23:19 -0500

    Directiva ngFor (ciclo para)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-servers]',
   template: `
-    <div class="app-server"></div>
-    <div class="app-server"></div>`,
+    <div class="app-server" *ngFor="let server of servers"></div>
+    <div class="app-server" [ngStyle]="{color: i%2==0 ? 'gray': 'violet'}" *ngFor="let server of servers; let i = index"></div>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+
+  servers = [
+    'server1',
+    'server2',
+    'server3',
+  ];
 
   constructor() { }
 
