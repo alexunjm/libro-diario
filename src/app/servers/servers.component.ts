1b46569d734820ecc476bdc9debd626d31108b88
alexun <alexunjm@gmail.com>
2017-05-19 21:01:44 -0500

    servicios e inyección de servicios en servicios

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ServerList } from '../services/server-list.service';
+import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: '[app-servers]',
   template: `
     <div class="app-server" appColor *ngFor="let server of servers"></div>
-    <div class="app-server" appBold [ngStyle]="{color: i%2==0 ? 'gray': 'violet'}" *ngFor="let server of servers; let i = index"></div>`,
-  styleUrls: ['./servers.component.css']
+    <div class="app-server" appBold [ngStyle]="{color: i%2==0 ? 'gray': 'violet'}" *ngFor="let server of servers; let i = index"></div>
+    <button (click)="add()">add</button>`,
+  styleUrls: ['./servers.component.css'],
+  providers: [ServerList, LoggingService]
 })
 export class ServersComponent implements OnInit {
 
-  servers = [
-    'server1',
-    'server2',
-    'server3',
-  ];
+  servers: any[];
 
-  constructor() { }
+  constructor(public sl: ServerList) {
+    this.servers = sl.servers;
+  }
 
   ngOnInit() {
+  }
+
+  add() {
+    this.sl.addServer('server' + this.servers.length);
   }
 
 }
---
+++ src/app/services/server-list.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { LoggingService } from './logging.service';
+
+@Injectable()
+export class ServerList {
+
+  public servers = [
+    'server1',
+    'server2',
+    'server3',
+  ];
+
+  constructor(public logServ: LoggingService) {}
+
+  addServer(serverName: string) {
+    this.servers.push(serverName);
+    this.logServ.log('server added: ' + serverName);
+  }
+}
